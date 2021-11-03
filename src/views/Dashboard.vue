f1102c32c9744dcb69cd6ce62926b90ce4c8d518
ben <tbeben77@gmail.com>
2021-11-03 22:50:39 +0900

    working on using firebase

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -22,7 +22,7 @@
     </ul>
     <div class="fixed-action-btn">
       <router-link to="/new" class="btn-floating btn-large red">
-        +
+        <i class="fa fa-plus"></i>
       </router-link>
     </div>
   </div>
