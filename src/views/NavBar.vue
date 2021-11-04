114d6696dd62c5dac512d2cf28b57b3b7256470c
tbeben <tbeben77@gmail.com>
2021-11-04 14:20:15 +0900

    add more stars

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -3,7 +3,7 @@
     <div class="nav-wrapper green">
       <div class="container">
         <router-link to="/" class="brand-logo"
-          >☆ Employee Manager ☆</router-link
+          >☆☆ Employee Manager ☆☆</router-link
         >
       </div>
     </div>
