290c886d1a8ccfe3f431dad586c60540a258d201
tbeben <tbeben77@gmail.com>
2021-11-04 13:28:09 +0900

    edit logo name

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="nav-wrapper green">
       <div class="container">
-        <router-link to="/" class="brand-logo">Employee Manager</router-link>
+        <router-link to="/" class="brand-logo">Employee Manager2</router-link>
       </div>
     </div>
   </nav>
